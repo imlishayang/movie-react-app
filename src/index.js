ade02323f519778fec912ff226299478c7d988a0
Lisa Yang <lisayang041@gmail.com>
2022-09-17 13:27:48 +0800

    render root

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import App from './App';
 // ReactDom no longer supported by React
 // ReactDOM.render(<App/>, document.getElementById('root'));
 
+
+//The root can be used to render a React element into the DOM with render:
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App/>);
